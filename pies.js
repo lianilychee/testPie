7b0c910aaa4ab2ac6b4a4681437fcda5b53270c2
greenteawarrior <emily.wang@students.olin.edu>
2014-06-29 17:23:06 -0400

    fixed jquery things

--- pies.js
+++ pies.js
@@ -1,12 +1,15 @@
 /* alert("hello world!"); */
 
-var vis = d3.select(".svg_donut");
-var arc = d3.svg.arc()
-	.innerRadius(50)
-	.outerRadius(100)
-	.startAngle(0)
-	.engAngle(1.5*Math.PI);
+$(document).ready(function () {
+    var vis = d3.select(".svg_donut");
 
-vis.append("path")
-	.attr("d",arc)
-	.attr("transform", "translate(300,200)"); 
+    var arc = d3.svg.arc()
+        .innerRadius(50)
+        .outerRadius(100)
+        .startAngle(0)
+        .endAngle(1.5*Math.PI);
+
+    vis.append("path")
+        .attr("d",arc)
+        .attr("transform", "translate(300,200)"); 
+})
