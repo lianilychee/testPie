e3e0e1f183754bd16345e92f20fe4f2cfa024990
Lye <liani.lye@str-mac-0011.str.corp>
2014-07-07 16:18:23 -0400

    end of workday

--- sunburst.js
+++ sunburst.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+/*
+// RINGS KINDA WORK
 var rawData = [{"rt": 0, "fav": 1},
 				{"rt": 0, "fav": 1},
 				{"rt": 1, "fav": 1},
@@ -15,13 +17,15 @@ var total = rawData.length;
 var overlap = rtCount + favCount - total;
 var dataset = {
 		rtCount: [rtCount, total-rtCount],
-		favCount: [favCount, total-favCount],
-};
-console.log(dataset);
+		favCount: [favCount, total-favCount] };
 
 var width = 600, height = 400, cwidth = 25;
 
-var color = d3.scale.category20();
+// var color = d3.scale.category20();
+var color = d3.scale.ordinal()
+	// blue, orange
+//	.range(["#5DA5DA", "#FFFFFF"]);
+	.range(["#5DA5DA", "#FAA43A"]);
 
 var pie = d3.layout.pie()
 	.sort(null);
@@ -39,10 +43,12 @@ var path = gs.selectAll("path")
 	.data(function(d) { return pie(d); })
   .enter().append("path")
 	.attr("fill", function(d, i) { return color(i); })
-	.attr("d", function(d, i, j) { return arc.innerRadius(10+cwidth*j).outerRadius(cwidth*(j+1))(d); });
+	.attr("d", function(d, i, j) { return arc.innerRadius(50+cwidth*j).outerRadius(cwidth*(j+1))(d); });
 });
+*/
 
-/*		
+	
+// ALL SEGMENTS IN ONE RING
 	var vis = d3.select("#sunburst");
 	var width = 600, height = 400;
 
@@ -64,36 +70,15 @@ var path = gs.selectAll("path")
 		.attr("transform", "translate(" +width/2+ "," +height/2+ ")");
 
 	// DATA NOT IN CSV FORM
-	/*
 	var dataset = [{"age": "< 5", "population": 5},
 					{"age": "5-13", "population": 10}];
 	console.log(dataset);
-	*/
+	
+//	dataset.forEach(function(d) {
+//		console.log("run d function");
+//		d.population = +d.population;
+//	});
 
-/*
-	var otherData = {
-		tweet1: ["< 5", 5],
-		tweet2: ["5-13", 10],
-	};
-	console.log(otherData);
-
-	var gs = svg.selectAll("g").data(d3.values(otherData)).enter().append("g");
-	var path = gs.selectAll("path")
-		.data(function(d) {return pie(d);})
-	.enter().append("path")
-		.attr("fill", function(d,i) {return color{i};})
-		.attr("d", function(d,i,j) {
-			return arc.innerRadius(10+cwidth*j).outerRadius(cwidth*(j+1))(d);
-		});
-
-	/*
-	dataset.forEach(function(d) {
-		console.log("run d function");
-		d.population = +d.population;
-	});
-	*/	
-
-/*
 	// Draw each arc, named g
 	var g = svg.selectAll(".arc")
 			.data(pie(dataset))
@@ -110,6 +95,6 @@ var path = gs.selectAll("path")
 		.attr("transform", function(d) {return "translate(" +innerArc.centroid(d)+ ")";})
 		.attr("dy", ".35em")
 		.style("text-anchor", "middle")
-		.text(function(d) {return d.data.age /* + d.data.population */  /*;});
+		.text(function(d) {return d.data.age /* + d.data.population */ ;});
 
-}); */
+});
