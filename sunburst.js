0430e7f8baaaea7a2998f94b295840fd88680b7c
Lye <liani.lye@str-mac-0011.str.corp>
2014-07-07 14:23:37 -0400

    Visual now filling correct SVG, rather than creating another.

--- sunburst.js
+++ sunburst.js
@@ -1,50 +1,54 @@
 $(document).ready(function () {
 		
-	console.log("SUNBURST");
-
 	var vis = d3.select("#sunburst");
 	var width = 600, height = 400;
 
 	var color = d3.scale.ordinal()
-		.range(["#5DA5DA", "#FAA43A"]);
+		.range(["#5DA5DA", "#FAA43A", "4D4D4D"]);
 
-	var arc = d3.svg.arc()
-		.innerRadius(0)
-		.outerRadius(200);
+	var innerArc = d3.svg.arc()
+		.innerRadius(50)
+		.outerRadius(100);
 
 	var pie = d3.layout.pie()
 		.sort(null)
-		.value(function(d) { return d.population; });
+		.value(function(d) {return d.population;});
 
-	var svg = d3.select("body").append("svg")
+	var svg = d3.select("#sunburst").append("svg")
 		.attr("width", width)
 		.attr("height", height)
 		.append("g")
-		.attr("transform", "translate(" +width/2+ "," +height/2 + ")");
+		.attr("transform", "translate(" +width/2+ "," +height/2+ ")");
 
-	/* DATA NOT IN CSV FORM */
-	var dataset = [{"age": "< 5", "population": 2704659},
-					{"age": "5-13", "population": 4499890}];
+	// DATA NOT IN CSV FORM
+	var dataset = [{"age": "< 5", "population": 5},
+					{"age": "5-13", "population": 10}];
 
 	console.log(dataset);
 
-	  dataset.forEach(function(d) {
+	/*
+	dataset.forEach(function(d) {
+		console.log("run d function");
 		d.population = +d.population;
-	  });
+	});
+	*/
 
-	  var g = svg.selectAll(".arc")
-		  .data(pie(dataset))
+	// Draw each arc, named g
+	var g = svg.selectAll(".arc")
+		 	.data(pie(dataset))
 		.enter().append("g")
-		  .attr("class", "arc");
+			.attr("class", "arc");
 
-	  g.append("path")
-		  .attr("d", arc)
-		  .style("fill", function(d) {return color(d.data.age);});
+	// Fill path (each segment) with appropriate color
+	g.append("path")
+		.attr("d", innerArc)
+		.style("fill", function(d) {return color(d.data.age);});
 
-	  g.append("text")
-		  .attr("transform", function(d) {return "translate(" +arc.centroid(d)+ ")";})
-		  .attr("dy", ".35em")
-		  .style("text-anchor", "middle")
-		  .text(function(d) {return d.data.age;});
+	// Add label within segment
+	g.append("text")
+		.attr("transform", function(d) {return "translate(" +innerArc.centroid(d)+ ")";})
+		.attr("dy", ".35em")
+		.style("text-anchor", "middle")
+		.text(function(d) {return d.data.age /* + d.data.population */ ;});
 
 });
