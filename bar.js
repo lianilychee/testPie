5ccde6829ae8610860b8f8d941222d0448bf6891
Liani <liani@lychee.(none)>
2014-07-08 18:40:40 -0400

    Data set up for bar chart.

--- bar.js
+++ bar.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+	// Create data
+	// Data has elements: id, rtCount
+
+	var dataset = [];
+
+	function getId() { return Math.floor(Math.random()*(20000-10000+1) + 10000); };
+	function getCount() { return Math.floor(Math.random()*(50-1+1) + 1); };
+
+
+	for (var i = 0; i < 10; i++) {
+		dataset.push({"id": getId(), "count": getCount()});
+	};
+
+	console.log(dataset);
+
 	// Select HTML defined SVG and set dimensions.
 	var vis = d3.select("#sunburst");
 	var width = 600, height = 400;
@@ -18,78 +33,6 @@ $(document).ready(function () {
 
 	var cleanData = [{"spreadType": "Retweeted", "tweets": rtCount},
 					{"spreadType": "Favorited", "tweets": favCount}];
-
-
-
-/** I AM HERE.
-	* I've already got a radian to linear scale conversion.
-	* Now, I have to get that conversion to translate into overlapping start/endAngles...
-	* ... without making my segments vanish. */
-
-
-
-	// Scale changed from radians to tweets.
-	var scale = d3.scale.linear().domain([0, rawData.length]).range([0, 2*Math.PI]);
-
-	// Define color scales.
-	var innerColor = d3.scale.ordinal()
-		.range(["#5DA5DA", "#FFFFFF"]);
-
-	var outerColor = d3.scale.ordinal()
-		.range(["#FFFFFF", "#FAA43A"]);
-
-	// Define arc inner and outer radii.
-	var innerArc = d3.svg.arc()
-		.innerRadius(50)
-		.outerRadius(100)
-
-	var outerArc = d3.svg.arc()
-		.innerRadius(100)
-		.outerRadius(150);
-
-	// Set sections to draw in the order that it appears in the data.
-	var pie = d3.layout.pie()
-		.sort(null)
-		.value(function(d) {return d.tweets;});
-
-	var svg = d3.select("#sunburst").append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.append("g")
-		.attr("transform", "translate(" +width/2+ "," +height/2+ ")");
-	
-//	dataset.forEach(function(d) {
-//		console.log("run d function");
-//		d.tweets = +d.tweets;
-//	});
-
-	// Draw each arc, named g
-	var g = svg.selectAll(".arc")
-			.data(pie(cleanData))
-		.enter().append("g")
-			.attr("class", "arc");
-
-	// Fill path (each segment) with appropriate color
-	g.append("path")
-		.attr("d", innerArc)
-		.style("fill", function(d) {return innerColor(d.data.spreadType);});
-
-	g.append("path")
-		.attr("d", outerArc)
-		.style("fill", function(d) {return outerColor(d.data.spreadType);});
-
-	// Add label within segment
-	g.append("text")
-		.attr("transform", function(d) {return "translate(" +innerArc.centroid(d)+ ")";})
-		.attr("dy", ".35em")
-		.style("text-anchor", "middle")
-		.text(function(d) {return d.data.spreadType;});
-
-	g.append("text")
-		.attr("transform", function(d) {return "translate(" +outerArc.centroid(d)+ ")";})
-		.attr("dy", ".35em")
-		.style("text-anchor", "middle")
-		.text(function(d) {return d.data.spreadType;});
 
 
 });
