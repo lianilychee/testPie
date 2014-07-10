37901f0f38d656ca377804413f6511818b6f87d6
Liani <liani@lychee.(none)>
2014-07-09 20:45:01 -0400

    Sunburst opacity achieved.

--- sunburst.js
+++ sunburst.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
 	var rawData = [{"type": "RT", "yes": 10},
 					{"type": "Fav", "yes": 12}];
 
+	// Convert radians to linear based on tweets.
+
 	// Select HTML defined SVG and set dimensions.
 	var width = 600, height = 400;
 
@@ -20,88 +22,22 @@ $(document).ready(function () {
 		.endAngle(1.0*Math.PI);
 
 	var favArc = d3.svg.arc()
-		.innerRadius(100)
-		.outerRadius(150)
+		.innerRadius(75)
+		.outerRadius(125)
 		.startAngle(0)
 		.endAngle(1.5*Math.PI);
 
 	vis.append("path")
 		.attr("d", rtArc)
+		.attr("fill", "#5DA5DA")
+		.attr("opacity", "0.75")
 		.attr("transform", "translate(" +(width/2)+ "," +(height/2)+ ")");
+
 
 	vis.append("path")
 		.attr("d", favArc)
+		.attr("fill", "#FAA43A")
+		.attr("opacity", "0.75")
 		.attr("transform", "translate(" +(width/2)+ "," +(height/2)+ ")");
-
-/*	I AM HERE.
-	* I've already got a radian to linear scale conversion.
-	* Now, I have to get that conversion to translate into overlapping start/endAngles...
-	* ... without making my segments vanish.
-
-
-
-	// Scale changed from radians to tweets.
-	var scale = d3.scale.linear().domain([0, rawData.length]).range([0, 2*Math.PI]);
-
-	// Define color scales.
-	var innerColor = d3.scale.ordinal()
-		.range(["#5DA5DA", "#FFFFFF"]);
-
-	var outerColor = d3.scale.ordinal()
-		.range(["#FFFFFF", "#FAA43A"]);
-
-	// Define arc inner and outer radii.
-	var innerArc = d3.svg.arc()
-		.innerRadius(50)
-		.outerRadius(100)
-
-	var outerArc = d3.svg.arc()
-		.innerRadius(100)
-		.outerRadius(150);
-
-	// Set sections to draw in the order that it appears in the data.
-	var pie = d3.layout.pie()
-		.sort(null)
-		.value(function(d) {return d.tweets;});
-
-	var svg = d3.select("#bar").append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.append("g")
-		.attr("transform", "translate(" +width/2+ "," +height/2+ ")");
-	
-//	dataset.forEach(function(d) {
-//		console.log("run d function");
-//		d.tweets = +d.tweets;
-//	});
-
-	// Draw each arc, named g
-	var g = svg.selectAll(".arc")
-			.data(pie(cleanData))
-		.enter().append("g")
-			.attr("class", "arc");
-
-	// Fill path (each segment) with appropriate color
-	g.append("path")
-		.attr("d", innerArc)
-		.style("fill", function(d) {return innerColor(d.data.spreadType);});
-
-	g.append("path")
-		.attr("d", outerArc)
-		.style("fill", function(d) {return outerColor(d.data.spreadType);});
-
-	// Add label within segment
-	g.append("text")
-		.attr("transform", function(d) {return "translate(" +innerArc.centroid(d)+ ")";})
-		.attr("dy", ".35em")
-		.style("text-anchor", "middle")
-		.text(function(d) {return d.data.spreadType;});
-
-	g.append("text")
-		.attr("transform", function(d) {return "translate(" +outerArc.centroid(d)+ ")";})
-		.attr("dy", ".35em")
-		.style("text-anchor", "middle")
-		.text(function(d) {return d.data.spreadType;}); */
-
 
 });
